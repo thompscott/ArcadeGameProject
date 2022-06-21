4c59ae4a16fc641d3d814cd94d3dff9c98ef7eea
thompscott <tho.sco.lisd@gmail.com>
2022-06-21 15:18:06 -0500

    Start commit

---
+++ gameLogic.js
@@ -0,0 +1,13 @@
+let col = 0; /*values 0-6 for each column*/
+let colcount =[5, 5, 5, 5, 5, 5, 5]; /*tracks number of pieces in each column*/
+colcount[col] = colcount[col] - 1;
+boardstart = [[null, null, null, null, null, null, null],
+              [null, null, null, null, null, null, null],
+              [null, null, null, null, null, null, null],
+              [null, null, null, null, null, null, null],
+              [null, null, null, null, null, null, null],
+              [null, null, null, null, null, null, null],]
+              
+function placePiece(col) {
+
+}
